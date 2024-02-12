8c70c836f72018ba03c8cabbdf0e4bade07a409c
Argos Maia <33440266+argosmaia@users.noreply.github.com>
2024-02-12 00:48:14 -0300

    Create ultrassonicAndMotor.ino

---
+++ ultrassonicAndMotor.ino
@@ -0,0 +1,92 @@
+#include <Servo.h>
+#include <HCSR04.h>
+
+Servo myservo;  // create servo object to control a servo
+UltraSonicDistanceSensor distanceSensor(11, 12);
+
+int pos = 0;    // variable to store the servo position
+
+const int motor1Pin1 = 2; // Porta digital para o primeiro pino do motor 1
+const int motor1Pin2 = 3; // Porta digital para o segundo pino do motor 1
+const int motor2Pin1 = 4; // Porta digital para o primeiro pino do motor 2
+const int motor2Pin2 = 5; // Porta digital para o segundo pino do motor 2
+
+void stop();
+void forward();
+void backwards();
+void turnRight();
+void turnLeft();
+
+void setup() {
+  Serial.begin(9600); 
+  delay(500);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+}
+
+void loop() {
+  Serial.println(distanceSensor.measureDistanceCm());
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(1);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(1);                       // waits 15 ms for the servo to reach the position
+  }
+
+  long duration, distance;
+  duration = distanceSensor.measureDistanceCm();
+  distance = (duration / 2) / 29.1; // Distância em centímetros
+
+  if (distance < 10) { // Se a distância for menor que 10 cm, pare o motor
+    stop(); 
+  } else if (distance > 30) { // Se a distância for maior que 30 cm, vire para a esquerda ou direita aleatoriamente
+    if (random(2) == 0) {
+      turnLeft();
+    } else {
+      turnRight();
+    }
+  } else { // Se a distância estiver entre 10 cm e 30 cm, mova o motor para frente
+    forward(); 
+  }
+}
+
+void forward() {
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+}
+
+void backwards() {
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+}
+
+void stop() {
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+}
