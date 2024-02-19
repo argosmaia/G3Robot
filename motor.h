e5d214cbf0ee2c668676c6930c244575567c1eae
Argos Maia <33440266+argosmaia@users.noreply.github.com>
2024-02-19 14:12:35 -0300

    Update motor.h

--- motor.h
+++ motor.h
@@ -2,12 +2,16 @@
 #define MOTOR_H
 #include <Arduino.h>
 
-const int motor1Pin1 = 3; // Porta digital para o primeiro pino do motor 1
-const int motor1Pin2 = 2; // Porta digital para o segundo pino do motor 1
-const int motor2Pin1 = 5; // Porta digital para o primeiro pino do motor 2
-const int motor2Pin2 = 4; // Porta digital para o segundo pino do motor 2
+const int motor1Pin1 = 5; // Porta digital para o primeiro pino do motor 1
+const int motor1Pin2 = 3; // Porta digital para o segundo pino do motor 1
+const int motor2Pin1 = 6; // Porta digital para o primeiro pino do motor 2
+const int motor2Pin2 = 11; // Porta digital para o segundo pino do motor 2
 
-void motorSetup(){
+const int vs = 75;
+//const global pra regulação da velocidade 
+
+
+void motorSetup() {
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
   pinMode(motor2Pin1, OUTPUT);
@@ -15,38 +19,38 @@ void motorSetup(){
 }
 
 void forward() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
+  analogWrite(motor1Pin1, vs);
+  analogWrite(motor1Pin2, LOW);
+  analogWrite(motor2Pin1, vs);
+  analogWrite(motor2Pin2, LOW);
 }
 
 void backwards() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
+  analogWrite(motor1Pin1, LOW);
+  analogWrite(motor1Pin2, vs);
+  analogWrite(motor2Pin1, LOW);
+  analogWrite(motor2Pin2, vs);
 }
 
 void turnRight() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
+  analogWrite(motor1Pin1, vs);
+  analogWrite(motor1Pin2, LOW);
+  analogWrite(motor2Pin1, LOW);
+  analogWrite(motor2Pin2, vs);
 }
 
 void turnLeft() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
+  analogWrite(motor1Pin1, LOW);
+  analogWrite(motor1Pin2, vs); 
+  analogWrite(motor2Pin1, vs);
+  analogWrite(motor2Pin2, LOW);
 }
 
 void stop() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
+  analogWrite(motor1Pin1, LOW);
+  analogWrite(motor1Pin2, LOW);
+  analogWrite(motor2Pin1, LOW);
+  analogWrite(motor2Pin2, LOW);
 }
 
 # endif
